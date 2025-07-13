3029ce3c0e58c6d241a4a55c566da6570a407d6f
Andrei De Vera <janandreicasaodevera@gmail.com>
2025-07-13 23:23:14 +0000

    Delete sapling directory

--- sapling/reset.ino
+++
@@ -1,9 +0,0 @@
-void resetProcess() {
-  Serial.println("===RESETTING===");
-  
-  digitalWrite(MOLD_OUT, 0);
-  delay(60000);
-  digitalWrite(MOLD_OUT, 1);
-
-  Serial.println("===DONE RESET===");
-}
